89c0ff56fabc5886fbd30fe1b8a000100fb913bc
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-08 20:34:31 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,7 @@
 
 def turn_count(board)
   tour = 0
+  
   board.each do |charac|
     if charac == "X" || charac == "O"
       tour += 1
@@ -9,9 +10,12 @@ def turn_count(board)
 end
 
 def current_player(board)
+  pers = "X"
   if turn_count(board) % 2 == 0
-  return "X"
+    pers == "X"
+  return pers
 else
-  return "O"
+  pers == "O"
+  return pers
 end
 end
