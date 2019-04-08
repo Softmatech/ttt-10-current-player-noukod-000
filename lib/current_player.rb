20f665a97a8b1d5cd43d9787093f7a095d4a24d6
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-08 20:51:50 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,9 +11,13 @@ def turn_count(board)
 end
 
 def current_player(board)
+  if turn_count(board) > 0
   if turn_count(board) % 2 == 0
   return "X"
 else
   return "O"
+else
+  return "X"
+end
 end
 end
